b2efbbd627e8d6a72a7b7c8013f408ce04fe3b79
LidiiaK <lida.kudriavtseva@gmail.com>
2022-08-14 18:18:40 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color")
+const p = document.querySelector(".color");
+btn.addEventListener("click", changeColor);
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+    p.innerHTML=getRandomHexColor();
+}
